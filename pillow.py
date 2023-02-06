60efe8342e80b3210c56d616abcee1dc6f57be22
appukuttyE4 <124507280+appukuttyE4@users.noreply.github.com>
2023-02-06 12:12:27 +0530

    Update and rename PS1.txt to pillow.py

--- pillow.py
+++ pillow.py
@@ -1,4 +1,5 @@
-﻿1. Write a program to read an Image and generate the negative of an Image. 
+
+#1. Write a program to read an Image and generate the negative of an Image. 
 from PIL import Image 
 import numpy as np 
 from PIL import ImageOps 
@@ -10,7 +11,9 @@ print(img.size)
 img_invert = ImageOps.invert(img) 
 img_invert.save('Z:\CV_Lab\Images\index_invert.jpg') 
 img_invert.show() 
-2. Read an image and apply thresholding to the image. (Convert to Grayscale and then to binary). 
+
+
+#2. Read an image and apply thresholding to the image. (Convert to Grayscale and then to binary). 
 from PIL import Image 
 import numpy as np 
 from PIL import ImageOps 
@@ -24,7 +27,9 @@ threshold = 0
 img_invert_threshold = img.point( lambda p: 0 if p < threshold else p ) threshold = 255
 img_invert_threshold = img.point( lambda p: 255 if p > threshold else p ) img_invert_threshold = img_invert_threshold.convert('1') 
 img_invert_threshold.save('Z:\CV_Lab\Images\img_invert_threshold.jpg') img_invert_threshold.show() 
-3. Capture video from the Camera / static video and store continuous frames. 
+
+
+#3. Capture video from the Camera / static video and store continuous frames. 
 import cv2 
 import os 
 video = cv2.VideoCapture("Z:/CV_Lab/Images/sample.mp4") try: 
@@ -44,7 +49,9 @@ else:
 break 
 video.release() 
 cv2.destroyAllWindows() 
-4. Subtract 2 continuous frames which you got from the previous question. 
+
+
+#4. Subtract 2 continuous frames which you got from the previous question. 
 from PIL import Image 
 import numpy as np 
 from PIL import ImageOps 
@@ -59,7 +66,9 @@ print(img2_arr)
 frame_diff = img1_arr - img2_arr 
 sub_image = Image.fromarray(frame_diff) 
 sub_image.show()
-5. Image addition 
+
+
+#5. Image addition 
 from PIL import Image 
 import numpy as np 
 from PIL import ImageOps 
@@ -72,7 +81,9 @@ print(img2_arr)
 addition = img1_arr + img2_arr 
 add_image = Image.fromarray(addition) 
 add_image.save('Z:/CV_Lab/Images/image_addition.jpg') add_image.show() 
-6. Image Subtraction 
+
+
+#6. Image Subtraction 
 from PIL import Image 
 import numpy as np 
 from PIL import ImageOps 
@@ -85,24 +96,30 @@ print(img2_arr)
 suntraction = img1_arr - img2_arr 
 sub_image = Image.fromarray(suntraction) 
 sub_image.save('Z:/CV_Lab/Images/image_suntraction.jpg') sub_image.show()
-7. Image Transformation 
-a. Translation 
+
+
+#7. Image Transformation 
+#a. Translation 
 from PIL import Image 
 import numpy as np 
 from PIL import ImageOps 
 img = Image.open("Z:\CV_Lab\Images\index.jpg") img.show() 
+
 #translate 
 out1 = img.point(lambda i: i * 0.5) 
 out1.save('Z:/CV_Lab/Images/image_translation.jpg') out1.show() 
-b. Rotation 
+
+#b. Rotation 
 from PIL import Image 
 import numpy as np 
 from PIL import ImageOps 
 img = Image.open("Z:\CV_Lab\Images\index.jpg") img.show() 
+
 #rotate 
 out3 = img.rotate(45) 
 out3.save('Z:/CV_Lab/Images/image_rotation.jpg') out3.show() 
-c. Resize 
+
+#c. Resize 
 from PIL import Image 
 import numpy as np 
 from PIL import ImageOps 
@@ -110,7 +127,9 @@ img = Image.open("Z:\CV_Lab\Images\index.jpg") img.show()
 #resize 
 out2 = img.resize((128, 128)) 
 out2.save('Z:/CV_Lab/Images/image_resizing.jpg') out2.show()
-8. ROI Selection 
+
+
+#8. ROI Selection 
 img = Image.open("Z:/CV_Lab/Images/tiger.jpg") img.show() 
 imageName="Z:/CV_Lab/Images/tiger.jpg" 
 with Image.open(imageName) as my_image: 
@@ -119,7 +138,9 @@ my_image_arr=np.asarray(my_image)
 cropped_image_arr=cropped_image_arr * 5 
 my_image_arr[100:150,40:200]=cropped_image_arr out=Image.fromarray(my_image_arr) 
 out.show() 
-9. Write your own procedure to apply thresholding to an image. 
+
+
+#9. Write your own procedure to apply thresholding to an image. 
 from PIL import Image 
 import numpy as np 
 from PIL import ImageOps 
@@ -134,7 +155,9 @@ img_arr[img_arr>127] = 255
 img_arr[img_arr<=127] = 0 
 image = Image.fromarray(img_arr) 
 image.show()
-10. Perform Logical operations like AND, OR, NOT on images. 
+
+
+#10. Perform Logical operations like AND, OR, NOT on images. 
 from PIL import Image 
 from PIL import ImageChops 
 import numpy as np 
